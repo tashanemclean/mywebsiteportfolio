0a122d64e4a0c3aed98268b907b00594a258ef41
Tashane Mclean <tashane.mclean@gmail.com>
2019-05-02 18:05:39 -0400

    updated and styled blog modal

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import axios from "axios";
+import DropzoneComponent from "react-dropzone-component"
 
 import RichTextEditor from "../forms/rich-text-editor"
 
@@ -10,12 +11,40 @@ export default class BlogForm extends Component {
         this.state = {
             title: "",
             blog_status: "",
-            content: ""
+            content: "",
+            featured_image: ""
         }
 
         this.handleChange = this.handleChange.bind(this)
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this)
+
+        this.componentConfig = this.componentConfig.bind(this)
+        this.djsConfig = this.djsConfig.bind(this)
+        this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this)
+
+        this.featuredImageRef = React.createRef()
+    }
+
+    componentConfig(){
+        return {
+            iconFiletypes: [".jpg", ".png"],
+            showFiletypeIcon: true,
+            postUrl: "https://httpbin.org/post"
+        }
+    }
+
+    djsConfig(){
+        return {
+            addRemoveLinks: true,
+            maxFiles: 1
+        }
+    }
+
+    handleFeaturedImageDrop(){
+        return {
+            addedfile: file => this.setState({ featured_image: file})
+        }
     }
 
     handleRichTextEditorChange(content) {
@@ -33,16 +62,26 @@ export default class BlogForm extends Component {
         formData.append("portfolio_blog[blog_status]", this.state.blog_status);
         formData.append("portfolio_blog[content]", this.state.content);
 
+        if (this.state.featured_image){
+        formData.append("portfolio_blog[featured_image]", this.state.featured_image);
+        }
+
         return formData;
     }
 
     handleSubmit(event){
         axios.post("https://tashanemclean.devcamp.space/portfolio/portfolio_blogs", this.buildForm(), {withCredentials:true})
         .then(response => {
+            if (this.state.featured_image){
+                this.featuredImageRef.current.dropzone.removeAllFiles()
+                }
             this.setState({
                 title: "",
-                blog_status: ""
+                blog_status: "",
+                content: "",
+                featured_image: ""
             })
+
             this.props.handleSuccessfulFormSubmission(response.data.portfolio_blog)
 
         }).catch(error => {
@@ -82,6 +121,17 @@ export default class BlogForm extends Component {
                     <RichTextEditor 
                     handleRichTextEditorChange={this.handleRichTextEditorChange} 
                     />
+                </div>
+
+                <div className="image-uploaders">
+                <DropzoneComponent
+                ref={this.featuredImageRef}
+                config={this.componentConfig()}
+                djsConfig={this.djsConfig()}
+                eventHandlers={this.handleFeaturedImageDrop()}
+                >
+                <div className="dz-message">Featured Image</div>
+                </DropzoneComponent>
                 </div>
 
                 <button className="btn">Save</button>
