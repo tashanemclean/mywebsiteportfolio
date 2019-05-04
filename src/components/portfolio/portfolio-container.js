ecb21135b4398a8caf25b320e2f12bb9eed3abb9
Tashane Mclean <tashane.mclean@gmail.com>
2019-05-03 20:31:38 -0400

    updated main styles

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -17,19 +17,27 @@ export default class PortfolioContainer extends Component {
     }
 
     handleFilter(filter) {
-        this.setState({
-            data: this.state.data.filter(item => {
-                return item.category === filter;
-            })
-        })
+        if (filter === "CLEAR_FILTERS") {
+            this.getPortfolioItems();
+        } else {
+            this.getPortfolioItems(filter)
     }
-    getPortfolioItems() {
+    }
+    getPortfolioItems(filter = null) {
         axios
             .get("https://tashanemclean.devcamp.space/portfolio/portfolio_items")
             .then(response => {
-                this.setState({
-                    data: response.data.portfolio_items
-                })
+                if (filter) {
+                    this.setState({
+                        data: response.data.portfolio_items.filter(item => {
+                            return item.category === filter;
+                        })
+                    })
+                } else {
+                    this.setState({
+                        data: response.data.portfolio_items
+                    })
+                }
             })
             .catch(error => {
                 console.log(error);
@@ -55,18 +63,26 @@ export default class PortfolioContainer extends Component {
         }
 
         return (
-            <div className="portfolio-items-wrapper">
-                <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
-                    eCommerce
-                </button>
-                <button className="btn" onClick={() => this.handleFilter("Technology")}>
-                    Technology
-                </button>
-                <button className="btn" onClick={() => this.handleFilter("Enterprise")}>
-                    Enterprise
-                </button>
+            <div className="homepage-wrapper">
+                <div className="filter-links">
+                <button className="btn" onClick={() => this.handleFilter("CLEAR_FILTERS")}>
+                        All
+                    </button>
+                    <button className="btn" onClick={() => this.handleFilter("eCommerce")}>
+                        eCommerce
+                    </button>
+                    <button className="btn" onClick={() => this.handleFilter("Technology")}>
+                        Technology
+                    </button>
+                    <button className="btn" onClick={() => this.handleFilter("Education")}>
+                        Education
+                    </button>
+                </div>
+                <div className="portfolio-items-wrapper">
 
-                {this.portfolioItems()}
+
+                    {this.portfolioItems()}
+                </div>
             </div>
         )
     }
--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -5,6 +5,10 @@ export default class PortfolioDetail extends Component{
     constructor(props){
         super(props);
 
+        this.state = {
+            portfolioItem: {}
+        }
+
     }
 
     componentWillMount(){
@@ -19,15 +23,50 @@ export default class PortfolioDetail extends Component{
         {withCredentials: true}
         )
         .then(response => {
-            console.log("res",response)
+            this.setState({
+                portfolioItem: response.data.portfolio_item
+            })
         }).catch(error => {
             console.log("get portfolio item error", error)
         })
     }
     render() {
+        const {
+            banner_image_url,
+            category,
+            description,
+            logo_url,
+            name,
+            thumb_image_url,
+            url
+        } = this.state.portfolioItem;
+
+        const bannerStyles = {
+            backgroundImage: "url(" + banner_image_url + ")",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center center"
+        }
+
+        const logoStyles = {
+            width: "200px"
+        }
+
         return (
-            <div>
-                <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+            <div className="portfolio-detail-wrapper">
+                <div className="banner" style={bannerStyles}>
+                    <img scr={logo_url} style={logoStyles}/>
+                </div>
+
+                <div className="portfolio-det-discription-wrapper">
+                    <div className="description">{description}</div>
+                </div>
+
+                <div className="bottom-content-wrapper">
+                    <a href={url} className="links" target="_blank">
+                    Visit {name}
+                    </a>
+                </div>
             </div>
         )
     }
